923529e55162835757320773f2f6d89964be24c4
Swiichy <adlpromail@gmail.com>
2022-07-22 00:36:49 +0200

    Add empty message for bookmarked list

--- client/src/pages/Bookmarked/Bookmarked.jsx
+++ client/src/pages/Bookmarked/Bookmarked.jsx
@@ -10,26 +10,41 @@ export default function Bookmarked() {
   const [data, setData] = useOutletContext();
   const [inputText, setInputText] = useState("");
 
+  const bookmarkedMovies = data.filter(
+    (item) => item.isBookmarked && item.category === "Movie"
+  );
+  const bookmarkedSeries = data.filter(
+    (item) => item.isBookmarked && item.category === "TV Series"
+  );
+
   return (
     <BookmarkedWrapper>
       <SearchBar setInputText={setInputText} data={data} />
       <ListWrapper>
-        <CardListLayout
-          data={data}
-          setData={setData}
-          inputText={inputText}
-          filtered={filteredMovies}
-          title={"Bookmarked Movies"}
-        />
+        {bookmarkedMovies.length ? (
+          <CardListLayout
+            data={data}
+            setData={setData}
+            inputText={inputText}
+            filtered={filteredMovies}
+            title={"Bookmarked Movies"}
+          />
+        ) : (
+          <h2>"Bookmarked Movies is empty"</h2>
+        )}
       </ListWrapper>
       <ListWrapper>
-        <CardListLayout
-          data={data}
-          setData={setData}
-          inputText={inputText}
-          filtered={filteredSeries}
-          title={"Bookmarked TV Series"}
-        />
+        {bookmarkedSeries.length ? (
+          <CardListLayout
+            data={data}
+            setData={setData}
+            inputText={inputText}
+            filtered={filteredSeries}
+            title={"Bookmarked TV Series"}
+          />
+        ) : (
+          <h2>"Bookmarked TV Series is empty"</h2>
+        )}
       </ListWrapper>
     </BookmarkedWrapper>
   );
--- client/src/pages/Bookmarked/Bookmarked.style.js
+++ client/src/pages/Bookmarked/Bookmarked.style.js
@@ -2,4 +2,13 @@ import styled from "styled-components";
 
 export const BookmarkedWrapper = styled.div`
   height: 100vh;
+
+  h2 {
+    font-weight: 300;
+    font-size: 32px;
+    line-height: 40px;
+    letter-spacing: -0.5px;
+    color: #ffffff;
+    padding-bottom: 32px;
+  }
 `;
